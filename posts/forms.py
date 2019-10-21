75c375bc296cf319edc427a06e2681f3db2a7f5a
mandaringit <compurain@gmail.com>
2019-10-21 14:08:33 +0900

    Post detail 스타일링

--- posts/forms.py
+++ posts/forms.py
@@ -9,6 +9,9 @@ class PostForm(forms.ModelForm):
 
 
 class CommentForm(forms.ModelForm):
+    content = forms.CharField(
+        label="", help_text="🤬 악플은 지양해주세요", widget=forms.TextInput(attrs={'placeholder': "댓글을 써주세요"}))
+
     class Meta:
         model = Comment
         fields = ['content']
