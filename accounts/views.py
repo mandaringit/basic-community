270bf4e71251f6eb877b8c9d344f167dd8477c33
mandaringit <compurain@gmail.com>
2019-10-21 09:48:39 +0900

    가입, 로그인,로그아웃 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,44 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 # Create your views here.
 
 
 def index(req):
     return render(req, 'accounts/index.html')
+
+
+def signup(req):
+    if req.user.is_authenticated:
+        return redirect('accounts:index')
+
+    if req.method == 'POST':
+        form = UserCreationForm(req.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(req, user)
+            return redirect('accounts:index')
+    else:
+        form = UserCreationForm()
+    return render(req, 'accounts/form.html', {'form': form})
+
+
+def login(req):
+    if req.user.is_authenticated:
+        return redirect('accounts:index')
+
+    if req.method == "POST":  # 인증만 하는 것이기 때문에 form.get_user로 유저 정보를 가져와야함
+        form = AuthenticationForm(req, req.POST)
+        if form.is_valid():
+            auth_login(req, form.get_user())
+            return redirect('accounts:index')
+    else:
+        form = AuthenticationForm()
+
+    return render(req, 'accounts/form.html', {'form': form})
+
+
+def logout(req):
+    auth_logout(req)
+    return redirect('accounts:index')
